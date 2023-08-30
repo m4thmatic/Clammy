84902d802250d1b116e29730cb76d1f9f6e53696
Avi Silverman <avi.silverman@gmail.com>
2023-08-29 22:51:58 -0400

    v0.2. Added logging results to file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Clammy
 Addon for AshitaXI v4 that displays clamming information.
+
+## Commands:
+### /clammy
+ `/clammy log true` *Turns on results logging - stores file in /addons/Clammy/logs* 
+ `/clammy log false` *Turns off results logging (default)* 
--- clammy.lua
+++ clammy.lua
@@ -23,57 +23,68 @@
 addon.author   = 'MathMatic';
 addon.name     = 'Clammy';
 addon.desc     = 'Clamming calculator: displays bucket weight, items in bucket, & approximate value.';
-addon.version  = '0.1';
+addon.version  = '0.2';
 
 require ('common');
 local imgui = require('imgui');
-
-
+local settings = require('settings');
 
 local clammingItems = {
 	{ item="Bibiki slug",				weight=3,	gil=7},
-	{ item="bibiki urchin", 			weight=6,	gil=750},
-	{ item="broken willow fishing rod",	weight=6,	gil=0},
-	{ item="coral fragment",			weight=6,	gil=1735},
-	{ item="high-quality crab shell",	weight=6,	gil=3312},
-	{ item="crab shell",				weight=6,	gil=392},
-	{ item="elshimo coconut", 			weight=6,	gil=44},
-	{ item="elm log", 					weight=6,	gil=390},
-	{ item="fish scales",				weight=3,	gil=23},
-	{ item="Goblin armor",				weight=6,	gil=0}, --200
-	{ item="Goblin mail", 				weight=6,	gil=0}, --1000
-	{ item="goblin mask", 				weight=6,	gil=0}, --500
+	{ item="Bibiki urchin", 			weight=6,	gil=750},
+	{ item="Broken willow fishing rod",	weight=6,	gil=0},
+	{ item="Coral fragment",			weight=6,	gil=1735}, --AH=~3000
+	{ item="High-quality crab shell",	weight=6,	gil=3312}, --AH=~4500
+	{ item="Crab shell",				weight=6,	gil=392},  --Make sure HQ version is listed above NQ for proper registering of item
+	{ item="Elshimo coconut", 			weight=6,	gil=44},
+	{ item="Elm log", 					weight=6,	gil=4000}, --AH*, NPC=390
+	{ item="Fish scales",				weight=3,	gil=23},
+	{ item="Goblin armor",				weight=6,	gil=200},  --AH*, NPC=0
+	{ item="Goblin mail", 				weight=6,	gil=1000}, --AH*, NPC=0
+	{ item="Goblin mask", 				weight=6,	gil=500},  --AH*, NPC=0
 	{ item="Hobgoblin bread", 			weight=6,	gil=91},
 	{ item="Hobgoblin pie", 			weight=6,	gil=153},
-	{ item="igneous rock", 				weight=35,	gil=178},
-	{ item="jacknife", 					weight=11,	gil=35},
-	{ item="lacquer tree log", 			weight=6,	gil=3578},
-	{ item="maple log", 				weight=6,	gil=15},
-	{ item="nebimonite", 				weight=6,	gil=53},
-	{ item="oxblood", 					weight=6,	gil=13250},
-	{ item="pamamas", 					weight=6,	gil=20},
-	{ item="pamtam kelp", 				weight=6,	gil=7},
-	{ item="pebble", 					weight=7,	gil=1},
-	{ item="petrified log", 			weight=6,	gil=2193},
-	{ item="quality pugil scales",		weight=6,	gil=253},
-	{ item="pugil scales", 				weight=3,	gil=23},
-	{ item="rock salt",					weight=6,	gil=3},
-	{ item="seashell", 					weight=6,	gil=29},
-	{ item="shall shell",				weight=6,	gil=307},
-	{ item="titanictus shell", 			weight=6,	gil=357},
-	{ item="tropical clam", 			weight=20,	gil=5100},
-	{ item="turtle shell", 				weight=6,	gil=1190},
-	{ item="uragnite shell", 			weight=6,	gil=1455},
-	{ item="vongola clam", 				weight=6,	gil=192},
-	{ item="white sand",				weight=7,	gil=250},
+	{ item="Igneous rock", 				weight=35,	gil=178},
+	{ item="Jacknife", 					weight=11,	gil=35},
+	{ item="Lacquer tree log", 			weight=6,	gil=6000}, --AH*, NPC=3578
+	{ item="Maple log", 				weight=6,	gil=15},
+	{ item="Nebimonite", 				weight=6,	gil=53},
+	{ item="Oxblood", 					weight=6,	gil=13250},
+	{ item="Pamamas", 					weight=6,	gil=20},
+	{ item="Pamtam kelp", 				weight=6,	gil=7},
+	{ item="Pebble", 					weight=7,	gil=1},
+	{ item="Petrified log", 			weight=6,	gil=2193}, --AH=~3500
+	{ item="Quality pugil scales",		weight=6,	gil=253},  --AH~=1000 --Note: For some reason using "High-quality" doesn't register properly for pugil scales, leave as "Quality" (unsure why)
+	{ item="Pugil scales", 				weight=3,	gil=23},   --Make sure HQ version is listed above NQ for proper registering of item
+	{ item="Rock salt",					weight=6,	gil=3},
+	{ item="Seashell", 					weight=6,	gil=29},
+	{ item="Shall shell",				weight=6,	gil=307},
+	{ item="Titanictus shell", 			weight=6,	gil=357},  --AH=~600
+	{ item="Tropical clam", 			weight=20,	gil=5100},
+	{ item="Turtle shell", 				weight=6,	gil=1190},
+	{ item="Uragnite shell", 			weight=6,	gil=1455},
+	{ item="Vongola clam", 				weight=6,	gil=192},
+	{ item="White sand",				weight=7,	gil=250},
 };
+
+local defaultConfig = T{
+	log = false,
+}
+local config = settings.load(defaultConfig);
+
 
 local bucketSize = 50;
 local weight = 0;
 local money  = 0;
 local bucket = {};
 local cooldown = 0;
+--local file = nil;
 
+local fileName = ('log_%s.txt'):fmt(os.date('%Y_%m_%d__%H_%M_%S'));
+local fileDir = ('%s\\addons\\Clammy\\logs\\'):fmt(AshitaCore:GetInstallPath());
+local filePath = fileDir .. fileName;
+
+--------------------------------------------------------------------
 local function emptyBucket()
 	bucketSize = 50;
 	weight = 0;
@@ -85,8 +96,41 @@ local function emptyBucket()
 end
 
 --------------------------------------------------------------------
+function openLogFile()
+	if (ashita.fs.create_directory(fileDir) ~= false) then
+		file = io.open(filePath, 'a');
+
+		if (file == nil) then
+			print("Clammy: Could not open log file.")
+		else
+			return file;
+		end
+	end
+end
+
+--------------------------------------------------------------------
+function closeLogFile(file)
+	if (file ~= nil) then
+		io.close(file)
+	end
+end
+
+--------------------------------------------------------------------
+function writeLogFile(item)
+	local file = openLogFile();
+
+	if (file ~= nil) then
+		fdata = ('%s, %s\n'):fmt(os.date('%Y-%m-%d %H:%M:%S'), item);
+		file:write(fdata);
+	end
+
+	closeLogFile(file);
+end
+
+--------------------------------------------------------------------
 ashita.events.register('load', 'load_cb', function()
 	emptyBucket();
+
 end);
 
 --------------------------------------------------------------------
@@ -105,6 +149,7 @@ ashita.events.register('command', 'command_cb', function (e)
     -- Block all related commands..
     e.blocked = true;
 
+	--[[ --DEBUG COMMANDS
 	if (#args == 2 and args[2]:any('reset')) then --manually empty the bucket
 		emptyBucket();
         return;
@@ -114,6 +159,24 @@ ashita.events.register('command', 'command_cb', function (e)
         weight = tonumber(args[3]);
         return;
     end
+
+	if (#args == 3 and args[2]:any('additem')) then --manually add item
+		print("adding item: " .. args[3]);
+        writeLogFile(args[3]);
+        return;
+    end
+	]]
+
+	if (#args == 3 and args[2]:any('log')) then --turns loggin on/off
+        if (args[3] == "true") then
+			config.log = true;
+		else
+			config.log = false;
+		end
+
+		settings.save();
+        return;
+    end
 end);
 
 --------------------------------------------------------------------
@@ -121,7 +184,6 @@ ashita.events.register('text_in', 'Clammy_HandleText', function (e)
     if (e.injected == true) then
         return;
     end
-    
 
 	if (string.match(e.message, "You return the")) then
 		emptyBucket();
@@ -147,6 +209,11 @@ ashita.events.register('text_in', 'Clammy_HandleText', function (e)
 				money = money + citem.gil;
 				bucket[idx] = bucket[idx] + 1;
 				cooldown =  os.clock() + 10.5;
+
+				if (config.log == true) then
+					writeLogFile(citem.item);
+				end
+
 				return;
 			end
 		end
@@ -187,6 +254,12 @@ ashita.events.register('d3d_present', 'present_cb', function ()
 		for idx,citem in ipairs(clammingItems) do
 			if (bucket[idx] ~= 0) then
 				imgui.Text(" - " .. clammingItems[idx].item .. " [" .. bucket[idx] .. "]");
+				imgui.SameLine();
+				local valTxt = "(" .. clammingItems[idx].gil * bucket[idx] .. ")"
+				local x, _  = imgui.CalcTextSize(valTxt);
+				imgui.SetCursorPosX(imgui.GetCursorPosX() + imgui.GetColumnWidth() - x - imgui.GetStyle().FramePadding.x);
+				imgui.Text(valTxt);
+
 			end
 		end
 
