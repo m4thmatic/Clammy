155db27b46476a8b10deea06dd5b809532de8e0f
DrifterX <nholmes1985@hotmail.com>
2025-10-02 20:31:38 -0700

    Updating version

--- clammy.lua
+++ clammy.lua
@@ -23,7 +23,7 @@
 addon.author   = 'MathMatic/DrifterX';
 addon.name     = 'Clammy';
 addon.desc     = 'Clamming calculator: displays bucket weight, items in bucket, & approximate value.';
-addon.version  = '1.2.4';
+addon.version  = '1.2.5';
 
 require('common');
 local const = require('constants');
